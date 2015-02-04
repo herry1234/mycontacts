8cb2f2464d5fde9d1820a315405ba38f928ac41f
Hailiang Wang <hailiangwang@gmail.com>
2015-02-03 22:52:56 -0800

    product url

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -1,6 +1,6 @@
 (function(window, angular, undefined) {'use strict';
 
-var urlBase = "http://localhost:3000/api";
+var urlBase = "/api";
 var authHeader = 'authorization';
 
 var module = angular.module("starter.services", ['ngResource']);
