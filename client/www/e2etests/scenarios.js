57b317033a6c785cf3ff457441c85819d6ab45dc
Herry Wang <tech.herry@gmail.com>
2015-02-03 17:42:14 -0800

    test login

--- client/www/e2etests/scenarios.js
+++ client/www/e2etests/scenarios.js
@@ -9,6 +9,12 @@ describe('my app', function() {
   it('should automatically redirect to /login when location hash/fragment is empty', function() {
     expect(browser.getLocationAbsUrl()).toMatch("http://localhost:3000/index.html#/login");
   });
+  it('should login successfully', function() {
+    element(by.model('credentials.username')).sendKeys('abcd');
+    element(by.model('credentials.password')).sendKeys('1234');
+    element(by.css('.ion-log-in')).click();
+    expect(browser.getLocationAbsUrl()).toMatch("http://localhost:3000/index.html#/tab/contacts");
+  });
 
 
   describe('contacts list', function() {
